6730ae4c5683d30116c1f539b11ec1b298635ac1
kkokkojeong <comgraphics11@gmail.com>
2024-01-16 21:42:40 +0900

    new function at raytracer

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -1,19 +1,29 @@
-use crate::light::Light;
 // https://doc.rust-kr.org/ch17-00-oop.html
 use crate::ray::Ray;
 use crate::sphere::Sphere;
+use crate::light::Light;
 
 pub struct Raytracer {
     pub width: i32,
     pub height: i32,
 
-    // sphere: Sphere,
-    // light: Light,
+    sphere: Sphere,
+    light: Light,
 }
 
 impl Raytracer {
-    pub fn new(width: i32, height: i32) -> Raytracer {
-        Raytracer { width, height }
+    pub fn new(width: i32, height: i32) -> Self {
+        let mut sphere = Sphere::new(cgmath::Point3::new(0.0, 0.0, 0.5), 0.5);
+        sphere.amb = cgmath::Vector3::new(0.0, 0.0, 0.0);
+        sphere.diff = cgmath::Vector3::new(0.0, 0.0, 1.0);
+        sphere.spec = cgmath::Vector3::new(1.0, 1.0, 1.0);
+        sphere.alpha = 9.0;
+        sphere.ks = 0.8;
+
+        // located back of screen
+        let light = Light{ pos: cgmath::Vector3::new(0.0, 0.0, -1.0) };
+
+        Raytracer { width, height, sphere, light }
     }
 
     pub fn tracy_ray(ray: Ray) -> cgmath::Point3<f32> {
