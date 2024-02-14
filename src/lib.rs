1f9a0239aff853106e1a860386513e9042774081
kkokkojeong <comgraphics11@gmail.com>
2024-02-14 21:53:29 +0900

    wgpu -state

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,16 @@ pub mod raytracer;
 pub mod sphere;
 pub mod hit;
 pub mod light;
+pub mod state;
+
+
+/*
+* WebAssembly (WASM) 시 도전!
+*/
+// #[cfg(target_arch="wasm32")]
+// use wasm_bindgen::prelude::*;
+//
+// #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+// pub fn run() {
+//
+// }
---
+++ src/state.rs
@@ -0,0 +1,44 @@
+use winit::event::WindowEvent;
+use winit::window::Window;
+
+struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    // The window must be declared after the surface so
+    // it gets dropped after it as the surface contains
+    // unsafe references to the window's resources.
+    window: Window,
+}
+
+
+impl State {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: Window) -> Self {
+        todo!()
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+
+
+}
